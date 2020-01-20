ba1cb81040cfbcf9d737c2fc0ea6760a7b6d9e66
再见田园犬 <tcstory@163.com>
2020-01-20 18:30:04 +0800

    [chore] adjust config to make it works in firefox extension

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <main id="q-app">
+    <router-view />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>hello</p>
+    <q-page class="flex flex-center">
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    </q-page>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
